efeaf9c23400e7d188685b78462f41f2c0beb233
cristatechnologies <tarak.thakarar@gmail.com>
2024-02-23 14:09:08 +0530

    feat: onEnter login functionality done

--- src/components/Helpers/InputCom.jsx
+++ src/components/Helpers/InputCom.jsx
@@ -8,13 +8,14 @@ export default function InputCom({
   value,
   inputClasses,
   error = false,
-  mandatory=false,
+  mandatory = false,
   labelClasses = "text-qgray text-[13px] font-normal",
   min,
   max,
   patternValidation,
   maxNumber,
-  minNumber
+  minNumber,
+  inputHandlerOnKey,
 }) {
   return (
     <div className="input-com w-full h-full">
@@ -23,7 +24,8 @@ export default function InputCom({
           className={`input-label capitalize block  mb-2 ${labelClasses || ""}`}
           htmlFor={name}
         >
-          {label} <span className={`${mandatory?"text-red-600":'hidden'}`}>*</span>
+          {label}{" "}
+          <span className={`${mandatory ? "text-red-600" : "hidden"}`}>*</span>
         </label>
       )}
       <div
@@ -35,13 +37,14 @@ export default function InputCom({
           placeholder={placeholder}
           value={value}
           onChange={inputHandler}
+          onKeyUp={inputHandlerOnKey}
           className={`input-field placeholder:text-sm text-sm px-6 text-dark-gray w-full h-full font-normal bg-white focus:ring-0 focus:outline-none ${
             inputClasses || ""
           }`}
           type={type}
           id={name}
           minLength={min}
-          maxLength={max} 
+          maxLength={max}
           pattern={patternValidation}
           min={minNumber}
           max={maxNumber}
